0c768256949e3d7aa4bf6f4512cb6f55066a87b7
Joaquin Montes <joaco.rm512@gmail.com>
2021-04-21 20:32:13 -0300

    Class 1 - Table initiation

---
+++ src/components/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="margin">
+    <table className='table'>
+      <thead>
+        <tr>
+          <td>
+          Name
+          </td>
+          <td>
+          Email
+          </td>
+          <td>
+          Url
+          </td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+          Joaquin
+          </td>
+          <td>
+          joaco.rm512@gmail.comm
+          </td>
+          <td>
+          www.justkahdri.com
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    </div>
+  );
+}
+
+export default App;
