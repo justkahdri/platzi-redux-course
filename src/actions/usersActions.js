d6c9032000404ce9c73edeeba07c032d5b2c680d
Joaquin Montes <joaco.rm512@gmail.com>
2021-04-26 21:36:54 -0300

    Class 8 - Types Files

--- src/actions/usersActions.js
+++ src/actions/usersActions.js
@@ -1,16 +1,19 @@
 import axios from 'axios';
+import { SET_USERS } from '../types';
 
 export const getUsers = () => async dispatch => {
-    const response = await axios.get('https://jsonplaceholder.cypress.io/users');
-    if (response.status === 200) {
-        dispatch({
-            type: 'SET_USERS', 
-            payload: response.data
-        });
-    } else {
-        dispatch({
-            type: 'ERROR', 
-            payload: response.status
-        });
+    try {
+        const response = await axios.get('https://jsonplaceholder.cypress.io/users');
+        if (response.status === 200) {
+            dispatch({
+                type: SET_USERS, 
+                payload: response.data
+            });
+        } else {
+            new Error(`Response status: ${response.status}`);
+        }
+    } catch (err) {
+        console.error(err);
     }
+    
 }
--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -1,10 +1,12 @@
+import { SET_USERS } from "../types";
+
 const INITIAL_STATE = {
     users: []
 };
 
 const usersReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
-        case 'SET_USERS':
+        case SET_USERS:
             return { ...state, users: action.payload};
         default:
             return state;
